c7ee081afeef2b2e9d28bd864e6b28dd25fb5f26
ShaneEverittMurphy <ShaneEverittMurphy@gmail.com>
2020-02-08 21:12:13 -0800

    Overwrote config file

--- .config/config.fish
+++
@@ -1,5 +0,0 @@
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-set -U fish_user_paths /snap/bin $fish_user_paths
-set -U fish_user_paths /home/shane/.cargo/bin $fish_user_paths
-
-
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,5 @@
-#My fish shell configuration
-
-alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME/"
-
-#not as useful on a small laptop screen
-#neofetch --os_arch off --kernel_shorthand on
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+set -U fish_user_paths /snap/bin $fish_user_paths
+set -U fish_user_paths /home/shane/.cargo/bin $fish_user_paths
 
 
