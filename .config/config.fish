eb8089cc8a1b91308bbfa80097cb68840a05605a
ShaneEverittMurphy <ShaneEverittMurphy@gmail.com>
2020-02-08 20:52:01 -0800

    Added config.fish

---
+++ .config/config.fish
@@ -0,0 +1,5 @@
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+set -U fish_user_paths /snap/bin $fish_user_paths
+set -U fish_user_paths /home/shane/.cargo/bin $fish_user_paths
+
+
