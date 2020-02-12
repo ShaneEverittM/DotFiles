6188a78ddff8f024f0863a7eba5be36ceb4af330
ShaneEverittMurphy <ShaneEverittMurphy@gmail.com>
2020-02-12 00:41:40 -0800

    Print tasks on terminal start

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,11 @@
+# Alias for a bare git repo used to track dotfiles
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+# adds cargo and snap packages to path
 set -U fish_user_paths /snap/bin $fish_user_paths
 set -U fish_user_paths /home/shane/.cargo/bin $fish_user_paths
 
+# show todo at startup
+task list
 
 
