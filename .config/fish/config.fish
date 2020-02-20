6b64b74083493f17888118fa745f30667e5271ea
ShaneEverittM <ShaneEverittMurphy@gmail.com>
2020-02-20 15:47:09 -0800

    added local bin to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,8 +4,7 @@ alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 # adds cargo and snap packages to path
 set -U fish_user_paths /snap/bin $fish_user_paths
 set -U fish_user_paths /home/shane/.cargo/bin $fish_user_paths
+set -U fish_user_paths /home/shane/.local/bin $fish_user_paths
 
-# show todo at startup
-task list
 
 
