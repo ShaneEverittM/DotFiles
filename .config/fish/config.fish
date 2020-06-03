893e7131636543b9815d9b6eb8e135ba608d6b35
ShaneEverittM <shaneemurphy146@gmail.com>
2020-06-03 13:58:29 -0700

    shortcuts and renamed desktops

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,23 @@ alias config="/usr/bin/git --git-dir=$HOME/.dotgit/ --work-tree=$HOME/"
 
 neofetch --os_arch off --kernel_shorthand on
 
+set -U fish_user_paths /usr/local/bin /usr/bin /bin
+
+if command -v exa > /dev/null
+	abbr -a l 'exa'
+	abbr -a ls 'exa'
+	abbr -a ll 'exa -l'
+	abbr -a lll 'exa -la'
+else
+	abbr -a l 'ls'
+	abbr -a ll 'ls -l'
+	abbr -a lll 'ls -la'
+end
+
+if command -v broot > /dev/null
+	abbr -a brh 'br -h'
+end
+
 function fish_greeting
 end
 
