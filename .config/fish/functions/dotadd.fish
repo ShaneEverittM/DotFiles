69ca8305ad68302c457ffa0663f3b91097c311e5
ShaneEverittM <shaneemurphy146@gmail.com>
2020-06-30 12:05:28 -0700

    todo

--- .config/fish/functions/dotadd.fish
+++ .config/fish/functions/dotadd.fish
@@ -5,6 +5,7 @@
 # Assumes you only have one config file of each
 # name in you config directory
 
+#unroll as many files as provided?
 function dotadd --description "dotadd <filename>"
 	if test (count $argv) -ne 1; or test argv[1] = "--help"
 		echo "Usage: dotadd <file in XDG_CONFIG_HOME>"	
