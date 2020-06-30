57ae1894ff11832b60cbee8533ee3e163d843bac
ShaneEverittM <shaneemurphy146@gmail.com>
2020-06-30 01:41:14 -0700

    moved functions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,25 +6,6 @@ alias dot="/usr/bin/git --git-dir=$HOME/.dotgit/ --work-tree=$HOME/"
 
 set -U fish_user_paths $HOME/.cargo/bin /usr/local/bin /usr/bin /bin 
 
-function dotadd --description "dotadd <filename>"
-	if test (count $argv) -ne 1; or test argv[1] = "--help"
-		echo "Usage: dotadd <file in XDG_CONFIG_HOME>"	
-	else
-		dot add (fd $argv $XDG_CONFIG_HOME)
-	end
-end
-
-function dotcommit --description "dotpush <filename> <message>"
-	if test (count $argv) -ne 2; or test argv[1] = "--help"
-		echo "Usage: dotadd <file in XDG_CONFIG_HOME>"	
-	else
-		# echo $argv[1]
-		# echo $argv[2]
-		dot commit (fd $argv[1] $XDG_CONFIG_HOME) -m $argv[2]
-	end
-end
-
-
 if command -v exa > /dev/null
 	abbr -a ls 'exa'
 	abbr -a ll 'exa -l'
