c2bdc4b5c886227484bba7a59cede6664dffe69a
ShaneEverittM <shaneemurphy146@gmail.com>
2020-06-30 01:29:23 -0700

    added cool functions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,20 +1,36 @@
-#My fish shell configuration
+## -------------------------------------------------------------------------- ##
+##                         My fish shell configuration                        ## 
+## -------------------------------------------------------------------------- ## 
 
-alias cfg="/usr/bin/git --git-dir=$HOME/.dotgit/ --work-tree=$HOME/"
-
-#export PATH="$HOME/.cargo/bin:$PATH"
+alias dot="/usr/bin/git --git-dir=$HOME/.dotgit/ --work-tree=$HOME/"
 
 set -U fish_user_paths $HOME/.cargo/bin /usr/local/bin /usr/bin /bin 
 
+function dotadd --description "dotadd <filename>"
+	if test (count $argv) -ne 1; or test argv[1] = "--help"
+		echo "Usage: dotadd <file in XDG_CONFIG_HOME>"	
+	else
+		dot add (fd $argv $XDG_CONFIG_HOME)
+	end
+end
+
+function dotcommit --description "dotpush <filename> <message>"
+	if test (count $argv) -ne 2; or test argv[1] = "--help"
+		echo "Usage: dotadd <file in XDG_CONFIG_HOME>"	
+	else
+		# echo $argv[1]
+		# echo $argv[2]
+		dot commit (fd $argv[1] $XDG_CONFIG_HOME) -m $argv[2]
+	end
+end
+
 
 if command -v exa > /dev/null
-	abbr -a l 'exa'
 	abbr -a ls 'exa'
-	abbr -a lls 'exa'
 	abbr -a ll 'exa -l'
 	abbr -a lll 'exa -la'
 else
-	abbr -a l 'ls'
+	abbr -a ls 'ls'
 	abbr -a ll 'ls -l'
 	abbr -a lll 'ls -la'
 end
@@ -29,8 +45,10 @@ if command -v nvim > /dev/null
 	abbr -a vim 'nvim'
 end
 
+
 function fish_greeting
 end
+
 
 
 
