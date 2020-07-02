6b77779383fe543d2300af02a418b2000c8f8891
ShaneEverittM <shaneemurphy146@gmail.com>
2020-07-02 01:35:31 -0700

    rename bar

--- .config/polybar/scripts/launch-polybar.sh
+++ .config/polybar/scripts/launch-polybar.sh
@@ -6,8 +6,8 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload --config="/home/shane/.config/polybar/polybar-config" example &
+    MONITOR=$m polybar --reload --config="/home/shane/.config/polybar/polybar-config" spartan &
   done
 else
-  polybar --reload example &
+  polybar --reload spartan &
 fi
