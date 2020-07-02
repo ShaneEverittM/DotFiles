4a566237d017db218a0fbd883a00b0663c475831
ShaneEverittM <shaneemurphy146@gmail.com>
2020-07-02 13:51:01 -0700

    back to plasma panel

--- .config/polybar/scripts/launch-polybar.sh
+++ .config/polybar/scripts/launch-polybar.sh
@@ -4,9 +4,15 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
+i=1
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload --config="/home/shane/.config/polybar/polybar-config" spartan &
+	if [ $i -eq 1 ]; then
+	    MONITOR=$m TRAY_POS=right polybar --reload --config="/home/shane/.config/polybar/polybar-config" spartan &
+	else 
+	    MONITOR=$m polybar --reload --config="/home/shane/.config/polybar/polybar-config" spartan &
+	fi
+	((i++))
   done
 else
   polybar --reload spartan &
