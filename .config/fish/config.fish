3afd15507279b693d9b8ccf0fa5a196140fc52c7
ShaneEverittM <shaneeverittmurphy@gmail.com>
2020-12-21 00:27:18 -0800

    Add deno path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,9 @@
 
 alias dot="/usr/bin/git --git-dir=$HOME/.dotgit/ --work-tree=$HOME/"
 
-set -U fish_user_paths $HOME/.cargo/bin /usr/local/bin /usr/bin /bin 
+set -U DENO_INSTALL "/home/shane/.deno"
+set -U fish_user_paths $DENO_INSTALL/bin $HOME/.cargo/bin /usr/local/bin /usr/bin /bin 
+
 
 if command -v exa > /dev/null
 	abbr -a ls 'exa'
@@ -25,6 +27,8 @@ end
 if command -v nvim > /dev/null
 	abbr -a vim 'nvim'
 end
+
+abbr -a mon2cam 'deno run --unstable -A -r -q https://raw.githubusercontent.com/ShayBox/Mon2Cam/master/src/mod.ts'
 
 
 function fish_greeting
